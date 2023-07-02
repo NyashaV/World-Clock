e1e610ffac91ab20f68fa7e116ab2b2eb912ba13
NyashaV <ny_vanessa@hotmail.co.uk>
2023-07-02 22:01:43 +0100

    Added function for the select cities

--- javascript/index.js
+++ javascript/index.js
@@ -1,41 +1,71 @@
 function updateTime() {
   let losAngelesElement = document.querySelector("#los-angeles");
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  let losAngelesTime = moment().tz("America/Los_Angeles");
+  if (losAngelesElement) {
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    let losAngelesTime = moment().tz("America/Los_Angeles");
 
-  losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
-  losAngelesTimeElement.innerHTML = losAngelesTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    losAngelesDateElement.innerHTML = losAngelesTime.format("MMMM Do YYYY");
+    losAngelesTimeElement.innerHTML = losAngelesTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
   let tokyoElement = document.querySelector("#tokyo");
-  let tokyoDateElement = tokyoElement.querySelector(".date");
-  let tokyoTimeElement = tokyoElement.querySelector(".time");
-  let tokyoTime = moment().tz("Asia/Tokyo");
+  if (tokyoElement) {
+    let tokyoDateElement = tokyoElement.querySelector(".date");
+    let tokyoTimeElement = tokyoElement.querySelector(".time");
+    let tokyoTime = moment().tz("Asia/Tokyo");
 
-  tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
-  tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+    tokyoDateElement.innerHTML = tokyoTime.format("MMMM Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let interlakenElement = document.querySelector("#interlaken");
-  let interlakenDateElement = interlakenElement.querySelector(".date");
-  let interlakenTimeElement = interlakenElement.querySelector(".time");
-  let interlakenTime = moment().tz("Europe/Zurich");
+  if (interlakenElement) {
+    let interlakenDateElement = interlakenElement.querySelector(".date");
+    let interlakenTimeElement = interlakenElement.querySelector(".time");
+    let interlakenTime = moment().tz("Europe/Zurich");
 
-  interlakenDateElement.innerHTML = interlakenTime.format("MMMM Do YYYY");
-  interlakenTimeElement.innerHTML = interlakenTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    interlakenDateElement.innerHTML = interlakenTime.format("MMMM Do YYYY");
+    interlakenTimeElement.innerHTML = interlakenTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
   let capeTownElement = document.querySelector("#cape-town");
-  let capeTownDateElement = capeTownElement.querySelector(".date");
-  let capeTownTimeElement = capeTownElement.querySelector(".time");
-  let capeTownTime = moment().tz("Africa/Johannesburg");
+  if (capeTownElement) {
+    let capeTownDateElement = capeTownElement.querySelector(".date");
+    let capeTownTimeElement = capeTownElement.querySelector(".time");
+    let capeTownTime = moment().tz("Africa/Johannesburg");
 
-  capeTownDateElement.innerHTML = capeTownTime.format("MMMM Do YYYY");
-  capeTownTimeElement.innerHTML = capeTownTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
+    capeTownDateElement.innerHTML = capeTownTime.format("MMMM Do YYYY");
+    capeTownTimeElement.innerHTML = capeTownTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div class="time">${cityTime.format(
+            "h:mm:ss [<small>]A[</small>]"
+          )}</div>
+        </div>`;
 }
 
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+
+citiesSelectElement.addEventListener("change", updateCity);
